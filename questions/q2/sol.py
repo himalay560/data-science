04a7932ef827916d2cb73c4bb4da52587305b132
himalay560 <46533179+himalay560@users.noreply.github.com>
2019-02-22 01:15:22 +0530

    Add files via upload

---
+++ questions/q2/sol.py
@@ -0,0 +1,12 @@
+import pandas as p
+import numpy as n
+
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, n.nan, 9, 20, 14.5, n.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+d = p.DataFrame(exam_data , index=labels)
+print("Summary of the basic information about this DataFrame and its data:")
+print(d.info())
---
+++ questions/q5/sol.py
@@ -0,0 +1,11 @@
+import pandas as p
+import numpy as n
+
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, n.nan, 9, 20, 14.5, n.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+d = p.DataFrame(exam_data , index=labels)
+print(d.iloc[[1,2,3,4],[0,2]])
